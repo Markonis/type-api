99341dda3f57272a5f8bf3e5ccc309d2871387f6
Marko Pavlović <marko@affine.tech>
2019-03-12 10:01:17 +0100

    Don't rely on isEqual in tests

--- src/spec/api/main.spec.ts
+++ src/spec/api/main.spec.ts
@@ -1,6 +1,5 @@
 import { setPaths, ApiEnpoint, resolvePath } from '../../api/main';
 import { assert } from 'chai';
-import { isEqual } from 'lodash';
 
 describe('setPaths', () => {
 	const api = {
@@ -18,37 +17,37 @@ describe('setPaths', () => {
 
 	it('does not modify the original object', () => {
 		setPaths(api);
-		assert(isEqual(api.parentA.childP.path, []));
-		assert(isEqual(api.parentA.parentB.childQ.path, []));
-		assert(isEqual(api.parentA.parentB.childR.path, []));
-		assert(isEqual(api.parentC.childX.path, []));
+		assert.deepEqual(api.parentA.childP.path, []);
+		assert.deepEqual(api.parentA.parentB.childQ.path, []);
+		assert.deepEqual(api.parentA.parentB.childR.path, []);
+		assert.deepEqual(api.parentC.childX.path, []);
 	});
 
 	it('sets the paths correctly', () => {
 		const result = setPaths(api);
-		assert(isEqual(result.parentA.childP.path, ['parentA', 'childP']));
-		assert(isEqual(result.parentA.parentB.childQ.path, ['parentA', 'parentB', 'childQ']));
-		assert(isEqual(result.parentA.parentB.childR.path, ['parentA', 'parentB', 'childR']));
-		assert(isEqual(result.parentC.childX.path, ['parentC', 'childX']));
+		assert.deepEqual(result.parentA.childP.path, ['parentA', 'childP']);
+		assert.deepEqual(result.parentA.parentB.childQ.path, ['parentA', 'parentB', 'childQ']);
+		assert.deepEqual(result.parentA.parentB.childR.path, ['parentA', 'parentB', 'childR']);
+		assert.deepEqual(result.parentC.childX.path, ['parentC', 'childX']);
 	});
 
 	describe('resolvePath', () => {
 		it('returns an empty array given root path', () => {
 			const initialized = setPaths(api);
 			const result = resolvePath(initialized);
-			assert(isEqual(result, []));
+			assert.deepEqual(result, []);
 		});
 
 		it('returns the correct path given object in the middle', () => {
 			const initialized = setPaths(api);
 			const result = resolvePath(initialized.parentA.parentB);
-			assert(isEqual(result, ['parentA', 'parentB']));
+			assert.deepEqual(result, ['parentA', 'parentB']);
 		});
 
 		it('returns the correct path given reference to the endpoint', () => {
 			const initialized = setPaths(api);
 			const result = resolvePath(initialized.parentA.parentB.childQ);
-			assert(isEqual(result, ['parentA', 'parentB', 'childQ']));
+			assert.deepEqual(result, ['parentA', 'parentB', 'childQ']);
 		});
 
 		it('throws an error given object which does not belong to the tree', () => {
